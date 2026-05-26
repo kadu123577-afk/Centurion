207649b4e2e8b6cd7d8c166300840b43ceaa60ab
Carlos Eduardo <carlos@licit.ai>
2026-05-26 14:06:50 -0300

    fix: vite multi-entry login + app

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import { resolve } from 'path'
 
 export default defineConfig({
   root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'public/index.html'),
+        login: resolve(__dirname, 'public/login.html'),
+        app:   resolve(__dirname, 'public/app.html'),
+      }
+    }
+  },
   resolve: {
     alias: {
       '/src': resolve(__dirname, 'src'),
     }
   },
-  build: { outDir: '../dist', emptyOutDir: true },
   server: { port: 3000, open: '/login.html', fs: { allow: ['..'] } }
 })
